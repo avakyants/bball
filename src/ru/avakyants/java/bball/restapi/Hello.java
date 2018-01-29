238683edc8dfed5a7035ca73d22ef0b3c5988bdb
tema14 <tema14@172.20.10.2>
2018-01-29 16:56:26 +0300

    test post json - err

--- src/ru/avakyants/java/bball/restapi/Hello.java
+++ src/ru/avakyants/java/bball/restapi/Hello.java
@@ -1,10 +1,15 @@
 package ru.avakyants.java.bball.restapi;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import ru.avakyants.java.bball.model.Game;
 
 @Path("/hello")
 public class Hello {
@@ -27,6 +32,14 @@ public class Hello {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String sayArrayHello() {
 		return "[{\"id\":1,\"visitor\":\"CLE\",\"home\":\"GSW\"},{\"id\":2,\"visitor\":\"BOS\",\"home\":\"LAL\"}]";
+	}
+	
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addData(Game game) {		
+		String result = "Game added: "+game;
+		return Response.status(201).entity(result).build();
 	}
 	
 	
